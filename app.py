4eb0327e5aa87a0850f0480076ad10c6ddb45c90
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2023-02-01 15:10:46 -0300

    section 8 - 94. JWT claims and authorization

--- app.py
+++ app.py
@@ -33,6 +33,12 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "298252173539318119663928400280829780751"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
