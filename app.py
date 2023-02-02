551329a90f0b617c96cc8c04ee0481de0f9bb801
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2023-02-02 16:21:48 -0300

    seciont 9 - Database Migrations with alembic and Flask-Migrate

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import secrets
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -28,7 +29,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "298252173539318119663928400280829780751"
@@ -89,9 +90,6 @@ def create_app(db_url=None):
                 ),
             401
         )
-
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
