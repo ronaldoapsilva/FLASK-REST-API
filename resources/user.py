e68d8c5b4e94a4dd3644a0e8ac81b7002ab48ea1
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2023-02-06 20:23:15 -0300

    section 11 - 116. How to send emails with Python and Mailgun

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
@@ -11,6 +13,15 @@ from schemas import UserSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv('MAILGUN_DOMAIN')
+    return requests.post(
+		f"https://api.mailgun.net/v3/{domain}/messages",
+		auth=("api", os.getenv('MAILGUN_API_KEY')),
+		data={"from": "Ronaldo <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body})
 
 @blp.route("/register")
 class UserRegister(MethodView):
